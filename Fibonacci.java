3f093f26e1c058ae7bdbca578a2b87714703a172
pdur6n <146977388+pdur6n@users.noreply.github.com>
2023-10-04 18:13:18 +0200

    Add files via upload

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,25 +1,60 @@
 class Fibonacci{
-	public static void main(String[] args){
-		fibonacci1();
-	
+	public static void main(String[] entradas){
+		int cantidad=8
+		fibonacci1(cantidad);
+		System.out.println();
+		fibonacci2(cantidad);
+		System.out.println();
+		fibonacci2(cantidad);
 	}
 	
 	/* 
 		Utilizamos el bucle for
 	*/
 	
-	public static void fibonacci1 (){
+	public static void fibonacci1(int cantidad){
 		int v0=0;
 		int v1=1;
+		System.out.println(v0+", ");
+		System.out.println(v1+", ");
 		for (int i=0;i<5;i++){
+		for(i++<cantidad;)
 			int vn=v1+v0;
 			v0=v1;
 			v1=vn;
-			System.out.println(vn);
+			System.out.println(vn+", ");
+		}
 	}
-	
-	public static void fibonacci2 (){}
-	public static void fibonacci3 (){}
+	/*
+		utilizamos el bucle while
+	*/
+	public static void fibonacci2(int cantidad){
+		int v0=0;
+		int v1=1;
+		int i=0
+		System.out.println(v0+", ");
+		System.out.println(v1+", ");
+		while(i++<cantidad){
+			int vn=v1+v0;
+			v0=v1;
+			v1=vn;
+			System.out.println(vn+", ");
+			
+		}
+		
+	}
+	public static void fibonacci3(int cantidad){
+		int v0=0;
+		int v1=1;
+		System.out.println(v0+", ");
+		System.out.println(v1+", ");
+		do{
+			int vn=v1+v0;
+			v0=v1;
+			v1=vn;
+			System.out.println(vn+", ");
+		}while(++i<cantidad);
+	}
 
 
 }
